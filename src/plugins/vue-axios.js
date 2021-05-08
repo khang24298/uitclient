c4d82336af8d054cd77f09d69e5e7676a4ed8d5c
khang24298 <khangnguyen24298@gmail.com>
2021-05-08 22:16:36 +0700

    Change route

--- src/plugins/vue-axios.js
+++ src/plugins/vue-axios.js
@@ -2,6 +2,6 @@ import Vue from 'vue'
 import axios from 'axios'
 import VueAxios from 'vue-axios'
 
-axios.defaults.baseURL = 'http://uitwork/api';
+axios.defaults.baseURL = 'http://52.14.146.87/api';
 
 Vue.use(VueAxios, axios)
