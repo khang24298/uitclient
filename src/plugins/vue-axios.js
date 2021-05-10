46263eb5bd633a32bdfed00debe964109eb34bc6
khang24298 <khangnguyen24298@gmail.com>
2021-05-10 23:36:41 +0700

    Change host

--- src/plugins/vue-axios.js
+++ src/plugins/vue-axios.js
@@ -2,6 +2,6 @@ import Vue from 'vue'
 import axios from 'axios'
 import VueAxios from 'vue-axios'
 
-axios.defaults.baseURL = 'http://52.14.146.87/api';
+axios.defaults.baseURL = 'http://ec2-52-14-146-87.us-east-2.compute.amazonaws.com/api';
 
 Vue.use(VueAxios, axios)
