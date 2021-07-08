79a5292e495f0b8be03f86c60c614f4e7042ec1d
khang24298 <khangnguyen24298@gmail.com>
2021-07-08 19:09:25 +0700

    Change backend url

--- src/plugins/vue-axios.js
+++ src/plugins/vue-axios.js
@@ -2,6 +2,6 @@ import Vue from 'vue'
 import axios from 'axios'
 import VueAxios from 'vue-axios'
 
-axios.defaults.baseURL = 'http://uitwork/api';
+axios.defaults.baseURL = 'http://ec2-13-59-14-237.us-east-2.compute.amazonaws.com/api';
 
 Vue.use(VueAxios, axios)
